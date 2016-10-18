9b5315d1c99ea4c194badc6537aa25553cc1617b
VidereLicet <videlicet8@gmail.com>
2016-10-18 10:17:52 -0700

    Update Compile

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -577,7 +577,7 @@ bool CheckPermissions(std::string strAddress, unsigned int nPort)
 			std::vector<std::string> strPort = Split(vCheck[3], ':');
 			vCheck[3] = strPort[0];
 			
-			unsigned int nPortCheck = stoi(strPort[1]);
+			unsigned int nPortCheck = std::stoi(strPort[1]);
 			if(nPort != nPortCheck)
 				return false;
 		}
