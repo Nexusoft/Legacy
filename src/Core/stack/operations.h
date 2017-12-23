6e6f72b4cf14ab44aae6f1c7fd48f17a692cb6c5
VidereLicet <videlicet8@gmail.com>
2017-12-23 00:03:59 -0700

    Advanced Contract Operations

--- src/Core/stack/operations.h
+++ src/Core/stack/operations.h
@@ -29,95 +29,93 @@ namespace Core
 	enum
 	{
 		/* State Storage */
-		WRITE_REGISTER   //WRITE_STATE <lexer-id>
-		READ_REGISTER    //READ_STATE  <lexer-id>
+		WRITE_REGISTER = 0,  //WRITE_STATE <lexer-id>
+		READ_REGISTER  = 1,   //READ_STATE  <lexer-id>
 		
-		/* Logic Flow */
-		IF     // if(boolean) {
-		ELSE   // } else {
-		ENDIF  // }
-		
-		
-		FOR    // for(var ; boolean ; operator) {
-		ENDFOR // }
+		/* Control Flow */
+		IF     = 2, // if(boolean) {
+		ELSE   = 3, // } else {
+		ENDIF  = 4, // }
 		
 		
-		WHILE     // while(boolean) {
-		ENDWHILE  // }
+		FOR    = 5, // for(var ; boolean ; operator) {
+		ENDFOR = 6, // }
+		
+		
+		WHILE     = 7, // while(boolean) {
+		ENDWHILE  = 8, // }
 		
 		
 		/* Data Typs. */
-		UINT8     //unsigned char
-		UINT16    //unsigned short
-		UINT32    //unsigned int
-		UINT64    //unsigned long long
-		UINT128   //uint128
-		UINT256   //uint256
-		UINT512   //uint512
-		UINT576   //uint576
-		UINT1024  //uint1024
+		UINT8     = 9, //unsigned char
+		UINT16    = 10, //unsigned short
+		UINT32    = 11, //unsigned int
+		UINT64    = 12, //unsigned long long
+		UINT128   = 13, //uint128
+		UINT256   = 14, //uint256
+		UINT512   = 15, //uint512
+		UINT576   = 16, //uint576
+		UINT1024  = 17, //uint1024
 		
-		BIGNUM    //CBigNum
+		BIGNUM    = 18, //CBigNum (openssl)
 		
-		STRING    //std::string or char*
-		BOOL      //bool
-		
-		
-		SINT8
-		SINT16
-		SINT32
-		SINT64
+		STRING    = 19, //std::string or char*
+		BOOL      = 20, //bool
 		
 		
-		/* Hashing */
-		SK32
-		SK64
-		SK256
-		SK512
-		SK576
-		SK1024
+		SINT8     = 21, //signed char
+		SINT16    = 22, //signed short
+		SINT32    = 23, //signed int
+		SINT64    = 24, //signed int64
+        
+        //RANGE 25 - 31 for extensibility
+		
+		
+		/* Hashing (Skien and Keccak) */
+		SK32      = 32,
+		SK64      = 33,
+		SK256     = 34,
+		SK512     = 35,
+		SK576     = 36,
+		SK1024    = 37,
 		
 		
 		/* Elliptic Curve */
-		ECDSA_VERIFY
-		ECDSA_SIGN
+		ECDSA_VERIFY = 38,
+		ECDSA_SIGN   = 39,
 		
 		
 		/* Network */
-		TIMESTAMP //Core::UnifiedTimestamp()
+		TIMESTAMP    = 40, //Core::UnifiedTimestamp()
 		
 		
 		/* Nexus Account Specific */
-		ADDRESS  //get the account by ID
-		BALANCE  //
-		CALLER 
-		CALLDATA
+		ADDRESS      = 41, //get the account by ID
+		BALANCE      = 42, //account balance by id <ID = UINT512>
+		CALLER       = 43, //get caller account by ID (the signchain contract that called this one
+		CALLDATA     = 44, //ability to send data into a contract level function (TODO: think on this more)
 		
+        //RANGE 45 - 63 for caller and account extensibility
 		
 		/* Bitwise */
-		OR
-		XOR 
-		AND 
-		NOT 
-		BOTH      //yes / no 
-		NONE      //not yes / not no
-		EQUALS 
-		RSHIFT
-		LSHIFT
+		BIT_OR           = 64, // c++ symbol |
+		BIT_XOR          = 65, // c++ symbol ^
+		BIT_AND          = 66, // c++ symbol &
+		BIT_RSHIFT       = 69, // c++ symbol >>
+		BIT_LSHIFT       = 70, // c++ symbol <<
+        BIT_FLIP         = 71, // c++ symbol ~
 		
 		
-		/* Mathematics */
-		ADD 
-		DIV
-		MUL 
-		SUB 
-		EXP       //EXP base exp (base ^ exp)
-		MOD       //MOD base div (base % div)
-		LOG       //LOG base num
-		
-		
-		/* Functions */
-		FUNCTION  //paramters 
+		/* Operators */
+		ADD       = 72, //ADD reg_a reg_b (+)
+		DIV       = 73, //DIV reg_a reg_b (/)
+		MUL       = 74, //MUL reg_a reg_b (*)
+		SUB       = 75, //SUB reg_a reg_B (-)
+		EXP       = 76, //EXP base exp (base ^ exp)
+		MOD       = 77, //MOD base div (base % div)
+		LOG       = 78 //LOG base num
+
+        //79 - 255 TO BE DETERMINED
 	}
 
 }
