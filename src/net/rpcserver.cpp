601757547cc949f3651664a4c2ad182120eb9ac3
resistance-is-futile <brian.smith@nexus.io>
2018-01-31 11:16:17 -0700

    Added LLD to the unix and osx makefiles, fixed the unspentbalance rpc command, it was causing a crash.

--- src/net/rpcserver.cpp
+++ src/net/rpcserver.cpp
@@ -2507,19 +2507,30 @@ namespace Net
 				"Returns the total amount of unspent Nexus for given address\n"
 				"This is a more accurate command than Get Balance.\n");
 
-		set<Wallet::NexusAddress> setAddress;
+		set<Wallet::NexusAddress> setOfAddresses;
 		if (params.size() > 0)
 		{
-			Array inputs = params[2].get_array();
-			BOOST_FOREACH(Value& input, inputs)
+			// Array inputs = params[2].get_array();
+			for(int i = 0; i < params.size(); i++)
 			{
-				Wallet::NexusAddress address(input.get_str());
-				if (!address.IsValid())
-					throw JSONRPCError(-5, string("Invalid Nexus address: ")+input.get_str());
-				if (setAddress.count(address))
-					throw JSONRPCError(-8, string("Invalid parameter, duplicated address: ")+input.get_str());
-			   setAddress.insert(address);
+				Wallet::NexusAddress address(params[i].get_str());
+				if (!address.IsValid()) {
+					throw JSONRPCError(-5, string("Invalid Nexus address: ")+params[i].get_str());
+				}
+				if (setOfAddresses.count(address)){
+					throw JSONRPCError(-8, string("Invalid parameter, duplicated address: ")+params[i].get_str()); 
+				}
+			   setOfAddresses.insert(address);
 			}
+			// BOOST_FOREACH(Value& input, inputs)
+			// {
+			// 	Wallet::NexusAddress address(input.get_str());
+			// 	if (!address.IsValid())
+			// 		throw JSONRPCError(-5, string("Invalid Nexus address: ")+input.get_str());
+			// 	if (setOfAddresses.count(address))
+			// 		throw JSONRPCError(-8, string("Invalid parameter, duplicated address: ")+input.get_str());
+			//    setOfAddresses.insert(address);
+			// }
 		}
 
 		vector<Wallet::COutput> vecOutputs;
@@ -2528,13 +2539,13 @@ namespace Net
 		int64 nCredit = 0;
 		BOOST_FOREACH(const Wallet::COutput& out, vecOutputs)
 		{
-			if(setAddress.size())
+			if(setOfAddresses.size())
 			{
 				Wallet::NexusAddress address;
 				if(!ExtractAddress(out.tx->vout[out.i].scriptPubKey, address))
 					continue;
 
-				if (!setAddress.count(address))
+				if (!setOfAddresses.count(address))
 					continue;
 			}
 			
