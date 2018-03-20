528ce9a8d59e0100fd4518264cb6b576a66019c5
bibbityjibbity <bibbityjibbity@users.noreply.github.com>
2018-03-20 02:44:42 -0400

    Increase speed and fix export location
    added -j2 to most building processes and changed export path from .bashrc to .bash_profile, since the MinGW64 window is a login tty. now qmake will be available as command each time you load MinGW64

--- win_build.sh
+++ win_build.sh
@@ -24,20 +24,20 @@ cd /c/installfiles/boost
 #Compile libdb
 cd /c/installfiles/libdb/build_unix
 ../dist/configure --prefix=/c/deps --enable-mingw --enable-cxx --disable-shared --disable-replication
-make
-make install
+make -j2
+make -j2 install
 #Compile libpng
 cd /c/installfiles/libpng
 make -f scripts/makefile.msys install-static prefix=/c/deps
 #Compile qrencode
 cd /c/installfiles/qrencode
 ./configure --enable-static --disable-shared --without-tools --prefix=/c/deps
-make install
+make -j2 install
 #Compile openssl
 cd /c/installfiles/openssl
 ./configure no-shared mingw64 -m64 --prefix=/c/deps
-make
-make install
+make -j2
+make -j2 install
 #Compile miniupnpc
 cmd /C "cd C:/installfiles/miniupnpc && mingw32-make -f Makefile.mingw init upnpc-static"
 cd /c/installfiles/miniupnpc
@@ -47,13 +47,13 @@ cp *.a /c/deps/lib
 cp *.exe /c/deps/bin
 #Compile qt
 cd /c/installfiles/qt
-./configure -release -opensource -confirm-license -static -platform win32-g++ -no-sql-sqlite -qt-zlib -qt-libpng -qt-libjpeg -no-style-fusion -style-windows -style-windowsvista -nomake examples -no-opengl -no-openssl -no-dbus -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtcharts -skip qtconnectivity -skip qtdatavis3d -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtnetworkauth -skip qtpurchasing -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qtsvg -skip qttranslations -skip qtvirtualkeyboard -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebglplugin -skip qtwebsockets -skip qtwebview -skip qtwinextras -skip qtx11extras -skip qtxmlpatterns -prefix "C:\deps" QMAKE_CFLAGS+="-m64" QMAKE_CXXFLAGS+="-m64" QMAKE_LFLAGS+="-m64"
-make
-make install
+./configure -release -opensource -confirm-license -static -platform win32-g++ -silent -no-sql-sqlite -qt-zlib -qt-libpng -qt-libjpeg -no-style-fusion -style-windows -style-windowsvista -nomake examples -no-opengl -no-openssl -no-dbus -skip qt3d -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtcharts -skip qtconnectivity -skip qtdatavis3d -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtlocation -skip qtmacextras -skip qtmultimedia -skip qtnetworkauth -skip qtpurchasing -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qtsvg -skip qttranslations -skip qtvirtualkeyboard -skip qtwayland -skip qtwebchannel -skip qtwebengine -skip qtwebglplugin -skip qtwebsockets -skip qtwebview -skip qtwinextras -skip qtx11extras -skip qtxmlpatterns -prefix "C:\deps" QMAKE_CFLAGS+="-w -m64" QMAKE_CXXFLAGS+="-w -m64" QMAKE_LFLAGS+="-w -m64"
+make -j2
+make -j2 install
 #Set dependent PATH environment variable and make it permanent in MinGW
 cd ~
-echo "export PATH=/c/deps/bin:$PATH" >> ~/.bashrc
-source ~/.bashrc
+echo "export PATH=/c/deps/bin:$PATH" >> ~/.bash_profile
+source ~/.bash_profile
 #Clear screen and alert user Nexus is now ready to compile
 cd /c/Nexus
 clear
