8b93b20a7e56c854229bf2cba668c70640460b42
bibbityjibbity <bibbityjibbity@users.noreply.github.com>
2018-03-24 05:28:40 -0400

    more typo fixes and remove apostophe

--- create_dmg.sh
+++ create_dmg.sh
@@ -7,6 +7,7 @@ fi
 brew install python@2
 pip2 install appscript
 
+clear
 echo ' '
 echo 'Building nexus-qt dmg file...'
 echo '(Do not close or move windows until after this script says it finished.)'
@@ -16,10 +17,10 @@ source ~/.bash_profile
 python release/macdeploy/macdeployqtplus nexus-qt.app -dmg -fancy release/macdeploy/fancy.plist
 
 echo ' '
-echo Finished Building nexus-qt dmg file
+echo 'Finished Building nexus-qt dmg file'
 echo ' '
 echo 'File can be found by opening Finder, clicking Go, then Home on'
 echo 'the menu bar. Then open the Nexus folder and you should see'
-echo 'a file called nexus-qt.dmg. Double clikc it and drag in the window'
-echo 'that appears to the Applications folder and you're finished.'
+echo 'a file called nexus-qt.dmg. Double click it and drag in the window'
+echo 'that appears to the Applications folder and you are finished.'
 echo ' '
