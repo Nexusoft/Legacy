14c7659a418089a93c70296bfa687db7c7238f9c
bibbityjibbity <bibbityjibbity@users.noreply.github.com>
2018-03-24 04:43:35 -0400

    Fix python install name
    Brew updated default python to 3, so changed the script to adjust to this.

--- create_dmg.sh
+++ create_dmg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -d "/usr/local/lib/python2.7/site-packages/appscript-1.0.1.dist-info" ]; then
-	brew install python
+	brew install python@2
 	pip2 install appscript
 fi
 
@@ -8,7 +8,8 @@ echo ' '
 echo 'Building nexus-qt dmg file...'
 echo '(Do not close or move windows until after this script says it finished.)'
 
-export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+echo 'export PATH="/usr/local/opt/python/libexec/bin:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
 python release/macdeploy/macdeployqtplus nexus-qt.app -dmg -fancy release/macdeploy/fancy.plist
 
 echo ' '
