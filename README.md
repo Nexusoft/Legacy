039a0897758ed3595d45eeddb2c8622c3805af5b
VidereLicet <colin@nexus.io>
2018-06-09 21:05:04 -0700

    Bump Versions 2.5

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Nexus Core - 0.2.4.4
+Nexus Core - 0.2.5.0
 =====================================
 
 nex·usˈneksəs' : a connection or series of connections linking two or more things.
--- src/core/version.cpp
+++ src/core/version.cpp
@@ -13,7 +13,7 @@
 const std::string CLIENT_NAME("Nexus");
 
 /* The database type used (Berklee DB or Lower Level Database) */
-const std::string CLIENT_VERSION("0.2.4.4");
+const std::string CLIENT_VERSION("0.2.5.0");
 
 #if defined USE_LLD
     const std::string CLIENT_DATABASE("[LLD]");
