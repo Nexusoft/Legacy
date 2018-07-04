575c818d8084f170a0afc2821990b1f65a504dfd
VidereLicet <colin@nexus.io>
2018-07-04 12:11:48 +0100

    DNS Seed Nodes

--- src/core/unifiedtime.cpp
+++ src/core/unifiedtime.cpp
@@ -1,9 +1,9 @@
 /*******************************************************************************************
- 
+
 			Hash(BEGIN(Satoshi[2010]), END(Sunny[2012])) == Videlicet[2014] ++
-   
+
  [Learn and Create] Viz. http://www.opensource.org/licenses/mit-license.php
-  
+
 *******************************************************************************************/
 
 #include "unifiedtime.h"
@@ -32,7 +32,7 @@ std::vector<Net::CAddress> SEED_NODES;
 std::map<std::string, int> MAP_TIME_DATA;
 
 /** Declarations for the DNS Seed Nodes. **/
-static const std::vector<std::string> DNS_SeedNodes = 
+static const std::vector<std::string> DNS_SeedNodes =
 {
 	"node1.nexusearth.com",
 	"node1.mercuryminer.com",
@@ -109,7 +109,10 @@ static const std::vector<std::string> DNS_SeedNodes =
 	"node18.mercuryminer.com",
 	"node19.mercuryminer.com",
 	"node20.mercuryminer.com",
-	"node21.mercuryminer.com"
+	"node21.mercuryminer.com",
+	"wallet5.ddns.net", //viod
+	"wallet7.ddns.net",
+	"wallet8.ddns.net"
 };
 
 /** Declarations for the DNS Seed Nodes. **/
@@ -164,7 +167,7 @@ int64 UniversalTime(int64 nTimestamp) { return nTimestamp - UNIFIED_AVERAGE_OFFS
 void InitializeUnifiedTime()
 {
 	printf("***** Unified Time Initialized to %i\n", UNIFIED_AVERAGE_OFFSET);
-	
+
 	CreateThread(ThreadUnifiedSamples, NULL);
 }
 
@@ -174,112 +177,112 @@ void ThreadUnifiedSamples(void* parg)
 {
 	/* set the proper Thread priorities. */
 	SetThreadPriority(THREAD_PRIORITY_ABOVE_NORMAL);
-	
+
 	/* Compile the Seed Nodes into a set of Vectors. */
 	SEED_NODES    = DNS_Lookup(fLispNet ? DNS_SeedNodes_LISPnet : (fTestNet ? DNS_SeedNodes_Testnet : DNS_SeedNodes));
-	
+
 	/* Iterator to be used to ensure every time seed is giving an equal weight towards the Global Seeds. */
 	int nIterator = -1;
-	
+
 	for(int nIndex = 0; nIndex < SEED_NODES.size(); nIndex++)
 		SEEDS.push_back(SEED_NODES[nIndex].ToStringIP());
-	
+
 	/* The Entry Client Loop for Core LLP. */
 	string ADDRESS = "";
 	LLP::CoreOutbound SERVER("", strprintf("%u", fLispNet ? LISPNET_CORE_LLP_PORT : (fTestNet ? TESTNET_CORE_LLP_PORT : NEXUS_CORE_LLP_PORT)));
-    
+
     //
-    
+
     /* Latency Timer. */
 	loop() {
 		try
 		{
 			Sleep(1000);
-			
-			
+
+
 			/* Randomize the Time Seed Connection Iterator. */
             nIterator ++;
 			if(nIterator == SEEDS.size())
                 nIterator = 0;
-			
+
 			/* Connect to the Next Seed in the Iterator. */
 			SERVER.IP = SEEDS[nIterator];
 			SERVER.Connect();
-			
-			
+
+
 			/* If the Core LLP isn't connected, Retry in 10 Seconds. */
 			if(!SERVER.Connected())
 			{
 				printf("***** Core LLP: Failed To Connect To %s:%s\n", SERVER.IP.c_str(), SERVER.PORT.c_str());
-				
+
 				continue;
 			}
 
-			
+
 			/* Use a CMajority to Find the Sample with the Most Weight. */
 			CMajority<int> nSamples;
-			
-			
+
+
 			/* Create Latency Timer Object. */
             LLP::Timer LatencyTimer;
             LatencyTimer.Reset();
-            
-            
+
+
             /* Request an initial offset from Unified Time servers. */
 			SERVER.GetOffset((unsigned int)GetLocalTimestamp());
-				
-				
+
+
 			/** Read the Samples from the Server. **/
 			while(SERVER.Connected() && !SERVER.Errors() && !SERVER.Timeout(5))
 			{
 				Sleep(1);
-			
+
 				SERVER.ReadPacket();
 				if(SERVER.PacketComplete())
 				{
 					LLP::Packet PACKET = SERVER.NewPacket();
-					
+
 					/* Add a New Sample each Time Packet Arrives. */
 					if(PACKET.HEADER == SERVER.TIME_OFFSET)
 					{
-                        
+
                         /* Calculate the Latency Round Trip Time. */
                         unsigned int nLatency = LatencyTimer.ElapsedMilliseconds();
-                        
+
                         /* Calculate this particular sample. */
 						int nOffset = bytes2int(PACKET.DATA) + (nLatency / 2000);
 						nSamples.Add(nOffset);
-						
+
                         /* Reset the Timer and request another sample. */
                         LatencyTimer.Reset();
 						SERVER.GetOffset((unsigned int)GetLocalTimestamp());
-						
+
 						if(GetArg("-verbose", 0) >= 2)
 							printf("***** Core LLP: Added Sample %i | Seed %s | Latency %u ms\n", nOffset, SERVER.IP.c_str(), nLatency);
 					}
-					
+
 					SERVER.ResetPacket();
 				}
-				
+
 				/* Close the Connection Gracefully if Received all Packets. */
 				if(nSamples.Samples() >= 11)
 				{
                     MAP_TIME_DATA[SERVER.IP] = nSamples.Majority();
-                    
+
 					SERVER.Close();
 					break;
 				}
 			}
-			
-			
+
+
 			/* If there are no Successful Samples, Try another Connection. */
 			if(nSamples.Samples() == 0)
 			{
 				printf("***** Core LLP: Failed To Get Time Samples.\n");
 				//SEEDS.erase(SEEDS.begin() + nIterator);
-				
+
 				//SERVER.Close();
-				
+
 				continue;
 			}
 
@@ -287,38 +290,38 @@ void ThreadUnifiedSamples(void* parg)
 			if((MAP_TIME_DATA.size() > 0))
 			{
 				fTimeUnified = true;
-                
+
                 /* Majority Object to check for consensus on time samples. */
                 CMajority<int> UNIFIED_MAJORITY;
-                
+
                 /* Info tracker to see the total samples. */
                 std::map<int, unsigned int> TOTAL_SAMPLES;
-                
+
                 /* Iterate the Time Data map to find the majority time seed. */
                 for(std::map<std::string, int>::iterator it=MAP_TIME_DATA.begin(); it != MAP_TIME_DATA.end(); ++it)
                 {
-                    
+
                     /* Update the Unified Majority. */
                     UNIFIED_MAJORITY.Add(it->second);
-                    
+
                     /* Increase the count per samples (for debugging only). */
                     if(!TOTAL_SAMPLES.count(it->second))
                         TOTAL_SAMPLES[it->second] = 1;
                     else
                         TOTAL_SAMPLES[it->second] ++;
                 }
-                
+
                 /* Set the Unified Average to the Majority Seed. */
                 UNIFIED_AVERAGE_OFFSET = UNIFIED_MAJORITY.Majority();
-				
+
 				if(GetArg("-verbose", 0) >= 1)
 					printf("***** %i Total Samples | %i Offset (%u) | %i Majority (%u) | %" PRId64 "\n", MAP_TIME_DATA.size(), nSamples.Majority(), TOTAL_SAMPLES[nSamples.Majority()], UNIFIED_AVERAGE_OFFSET, TOTAL_SAMPLES[UNIFIED_AVERAGE_OFFSET], GetUnifiedTimestamp());
 			}
-            
+
             Sleep(30000);
-            
+
             continue;
-            
+
 		}
 		catch(std::exception& e){ printf("UTM ERROR: %s\n", e.what()); }
 	}
@@ -339,21 +342,20 @@ std::vector<Net::CAddress> DNS_Lookup(const std::vector<std::string>& DNS_Seed)
             BOOST_FOREACH(Net::CNetAddr& ip, vaddr)
             {
                 Net::CAddress addr = Net::CAddress(Net::CService(ip, Net::GetDefaultPort()));
-                
+
                 //Randomize the Seed Node Penalty time (3 - 7 days).
                 addr.nTime = GetUnifiedTimestamp() - (3 * 86400) - GetRand(7 * 86400);
-                
+
                 vNodes.push_back(addr);
-				
+
 				printf("DNS Seed: %s\n", addr.ToStringIP().c_str());
-				
+
                 //time penalty for seed nodes
 				Net::addrman.Add(addr, ip, true);
             }
         }
 	printf("DNS Seed Count: %d\n",scount);
     }
-	
+
 	return vNodes;
 }
-
