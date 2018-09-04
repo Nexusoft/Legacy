bce2ea14f94b5d7b529220e92cbd32ee54c557b1
VidereLicet <colin@nexus.io>
2018-09-03 23:38:26 -0700

    Mining Server Prime Min Origins

--- src/LLP/miningserver.h
+++ src/LLP/miningserver.h
@@ -496,7 +496,7 @@ namespace LLP
                 Core::CBlock NEW_BLOCK = BASE_BLOCK;
 
                 /* Checks for prime min origins. */
-                for(int i = 0; ; i++)
+                for(int i = 1; ; i++)
                 {
                     /* Create a vin script sig that will change merkle root */
                     NEW_BLOCK.vtx[0].vin[0].scriptSig = (Wallet::CScript() << (i * 1024 * (MAP_BLOCKS.size() + 1)));
