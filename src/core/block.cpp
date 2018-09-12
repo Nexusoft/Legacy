1231d39c4ca9506394bc76e69201bcc9de19f63a
VidereLicet <colin@nexus.io>
2018-09-11 23:24:39 -0700

    Trust Key Hash Cleanup

--- src/core/block.cpp
+++ src/core/block.cpp
@@ -371,13 +371,6 @@ namespace Core
             if(!mapBlockIndex.count(trustKey.hashGenesisBlock))
                 return error("ConnectBlock() : Genesis Block Not Found.");
 
-            /* Don't allow Blocks Created without First Input Previous Output hash of Trust Key Hash.
-                This Secures and Anchors the Trust Key to all Descending Trust Blocks of that Key. */
-            if(vtx[0].vin[0].prevout.hash != trustKey.GetHash())
-            {
-                return error("ConnectBlock() : Trust Block Input Hash Mismatch to Trust Key Hash\n%s\n%s", vtx[0].vin[0].prevout.hash.ToString().c_str(), trustKey.GetHash().ToString().c_str());
-            }
-
             /* Read the Genesis Transaction's Block from Disk. */
             CBlock blockGenesis;
             if(!blockGenesis.ReadFromDisk(mapBlockIndex[trustKey.hashGenesisBlock], true))
