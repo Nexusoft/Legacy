f7d2e408a457b949851b2a89a903cb16c2024e57
bibbityjibbity <bibbityjibbity@users.noreply.github.com>
2018-09-16 12:19:02 -0400

    Fix previous removal
    Removed more than intended. Corrected so loop is continued but values still not removed from SEED vector

--- src/core/unifiedtime.cpp
+++ src/core/unifiedtime.cpp
@@ -263,6 +263,15 @@ void ThreadUnifiedSamples(void* parg)
             SERVER.Connect();
 
 
+            /* If the Core LLP isn't connected, Retry in 10 Seconds. */
+            if(!SERVER.Connected())
+            {   
+                /* Debug output. */
+                printf("***** Core LLP: Failed To Connect To %s:%s\n", SERVER.IP.c_str(), SERVER.PORT.c_str());
+                 
+                continue;
+             }
+            
             /* Use a CMajority to Find the Sample with the Most Weight. */
             CMajority<int> nSamples;
 
