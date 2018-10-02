5ffa370f17a2cb197cd95d5c2a2ea8adcbeb1dfd
bibbityjibbity <bibbityjibbity@users.noreply.github.com>
2018-10-02 10:49:50 -0400

    Update version to signify beta branch

--- src/core/version.cpp
+++ src/core/version.cpp
@@ -13,7 +13,7 @@
 const std::string CLIENT_NAME("Nexus");
 
 /* The database type used (Berklee DB or Lower Level Database) */
-const std::string CLIENT_VERSION("0.2.5.1-sync_patch");
+const std::string CLIENT_VERSION("0.2.5.2-beta");
 
 #if defined USE_LLD
     const std::string CLIENT_DATABASE("[LLD]");
