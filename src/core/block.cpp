f36b92f8bba07ccd93b0c22aaa9f8d6a41cfd0a7
VidereLicet <colin@nexus.io>
2019-01-01 10:04:09 -0700

    Check block on connect

--- src/core/block.cpp
+++ src/core/block.cpp
@@ -231,9 +231,9 @@ namespace Core
 
     bool CBlock::ConnectBlock(LLD::CIndexDB& indexdb, CBlockIndex* pindex)
     {
-        /* Double check channel range here. */
-        if (GetChannel() > 2)
-            return error("ConnectBlock() : Channel out of Range.");
+        /* Double check block here. */
+        if (!CheckBlock())
+            return error("ConnectBlock() : Connect Block failed");
 
         // Do not allow blocks that contain transactions which 'overwrite' older transactions,
         // unless those are already completely spent.
